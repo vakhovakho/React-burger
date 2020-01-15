7b76aac1eab45d679ac1d75c448c8e9f61068095
Vakho Nakashidze <vakho@Vakhos-MacBook-Pro.local>
2020-01-15 18:07:06 +0400

    Folder structure and modal should update hook. Section finished (Lesson 188)

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import Aux from '../../../hoc/Aux';
+import Aux from '../../../hoc/Aux/Aux';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map( igKey => {
-            return (
-                <li key = {igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li> );
-        })
-    return (
-        <Aux>
-            <h3> Your Order </h3>    
-            <p>A delicious bureger with following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-    <p><strong>Total Price:</strong> ${props.price.toFixed(2)}</p>
-            <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked = {props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType='Success' clicked = {props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )
-};
+class OrderSummary extends Component {
+    // This could be a functional component
+    componentDidUpdate() {
+        console.log('[OrderSummary] Will Update');
+    }
 
-export default orderSummary;
+    render () {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map( igKey => {
+                return (
+                    <li key = {igKey}>
+                        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                    </li> );
+            });
+
+        return (
+            <Aux>
+                <h3> Your Order </h3>    
+                <p>A delicious bureger with following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price:</strong> ${this.props.price.toFixed(2)}</p>
+                <p>Continue to Checkout?</p>
+                <Button btnType='Danger' clicked = {this.props.purchaseCancelled}>CANCEL</Button>
+                <Button btnType='Success' clicked = {this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
+}
+
+export default OrderSummary;
